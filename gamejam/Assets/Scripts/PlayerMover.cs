e93d695352603a9d6b418a6c17e703f727555ed7
VoyagerNorma <89327711+VoyagerNorma@users.noreply.github.com>
2021-08-23 15:37:55 -0400

    Inventory and Player Mover

---
+++ gamejam/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+    private KeyCode sprintKey = KeyCode.LeftShift;
+
+    public float speed = 1;
+    public float sprintSpeed = 1;
+    
+    private float lookSpeed = 5;
+
+    public GameObject placeholderBuilding;
+
+    public Transform placePnt;
+
+    private int attackMode = 1;
+
+    private Rigidbody rb;
+
+    private bool sprinting = false;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        MouseClicks();
+        TestNewMode();
+        RotatePlayer();
+        MovePlayer();
+        TestSprint();
+    }
+
+    private void MovePlayer() {
+        float zInput = Input.GetAxis("Vertical");
+        float xInput = Input.GetAxis("Horizontal");
+
+        Vector3 newVel = new Vector3(xInput, 0, zInput);
+        if (sprinting) {
+            rb.AddForce(newVel * sprintSpeed, ForceMode.Force);
+        }
+        else {
+            rb.AddForce(newVel * speed, ForceMode.Force);
+        }
+    }
+
+    private void RotatePlayer() {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Construct a ray from the current mouse coordinates
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            var point = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            transform.LookAt(point, transform.up);
+        }
+    }
+
+    private void TestNewMode() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            attackMode = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            attackMode = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            attackMode = 3;
+        }
+    }
+
+    private void MouseClicks() {
+        if (attackMode == 2 && Input.GetMouseButtonDown(0)) {
+            
+        }
+        if (attackMode == 3 && Input.GetMouseButtonDown(0)) {
+            PlaceBuilding();
+        }
+    }
+
+    private void PlaceBuilding() {
+        Instantiate(placeholderBuilding, placePnt.position, Quaternion.identity);
+    }
+
+    private void Attack() {
+
+    }
+
+    private void TestSprint() {
+        if (Input.GetKey(sprintKey)) {
+            sprinting = true;
+        }
+        else {
+            sprinting = false;
+        }
+    }
+}
---
+++ gamejam/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+public class UIHandler : MonoBehaviour {
+    public Canvas canvas;
+
+    private KeyCode inventoryKey = KeyCode.E;
+
+    private Transform currentItem;
+    public Transform inventory;
+    public GameObject inventorySlots;
+
+    private bool itemCanMove = false;
+    private bool slotTaken = false;
+
+    public Transform[] items;
+    private Transform takeSlot;
+    private Transform placeSlot;
+    private Transform replacingItem;
+    private void Start() {
+        inventory.gameObject.SetActive(false);
+    }
+
+    private void Update() {
+        MoveItem();
+        if (Input.GetKeyDown(inventoryKey)) {
+            inventory.gameObject.SetActive(!inventory.gameObject.activeSelf);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            inventory.gameObject.SetActive(false);
+        }
+        CheckClick();
+    }
+
+    private void CheckClick() {
+        if (Input.GetMouseButtonDown(0)) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Construct a ray from the current mouse coordinates
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider != null) {
+                    if (hit.collider.transform.tag == "Slot") {
+                        print("Hit");
+                        ProcessGrab(hit.collider.transform);
+                    }
+                    else {
+                        if (currentItem != null) { currentItem = null; }
+                    }
+                }
+                else {
+                    if (currentItem != null) { currentItem = null; }
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0)) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Construct a ray from the current mouse coordinates
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider != null) {
+                    if (hit.collider.transform.tag == "Slot") {
+                        ProcessPlace(hit.collider.transform);
+                    }
+                    else {
+                        if (currentItem != null) {
+                            itemCanMove = false;
+                            currentItem.localPosition = Vector3.zero;
+                            takeSlot = null;
+                            placeSlot = null;
+                        }
+                    }
+                }
+            }
+        else {
+                //when you let go of the mouse and its not over another slot: just returns the item to the slot you got it from
+                if (currentItem != null) {
+                    itemCanMove = false;
+                    currentItem.localPosition = Vector3.zero;
+                    takeSlot = null;
+                    placeSlot = null;
+                }
+            }
+        }
+    }
+
+    private void ProcessGrab(Transform slot) {
+        try {
+            currentItem = slot.GetChild(0).GetComponent<Transform>();
+            takeSlot = slot;
+            itemCanMove = true;
+        }
+        catch {
+            currentItem = null;
+        }
+    }
+
+    private void MoveItem() {
+        if (itemCanMove) {
+            //Vector3 mousePos = Input.mousePosition;
+            //currentItem.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, canvas.worldCamera, out pos);
+            currentItem.position = canvas.transform.TransformPoint(pos);
+        }
+    }
+
+    private void ProcessPlace(Transform slot) {
+        foreach (Transform item in items) {
+            if (item.position == slot.position) {
+                replacingItem = item;
+                slotTaken = true;
+            }
+        }
+        if (!slotTaken && currentItem != null) {
+            itemCanMove = false;
+            currentItem.SetParent(slot);
+            currentItem.localPosition = Vector3.zero;
+            placeSlot = slot;
+            takeSlot = null;
+            placeSlot = null;
+
+        }
+        else if (currentItem != null){
+            slotTaken = false;
+            itemCanMove = false;
+            currentItem.SetParent(slot);
+            placeSlot = slot;
+            currentItem.localPosition = Vector3.zero;
+            replacingItem.SetParent(takeSlot);
+            replacingItem.localPosition = Vector3.zero;
+            takeSlot = null;
+            placeSlot = null;
+        }
+    }
+}
